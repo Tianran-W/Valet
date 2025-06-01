935c1a5d5479371a3ea12a5251007b573773fcdd
Tianran-W <529708894@qq.com>
2025-06-01 15:33:39 +0800

    docs: :memo: 添加开源协议和版权声明

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# valet
+# Valet
 
-A new Flutter project.
+一个用于设备管理的Flutter应用。
 
-## Getting Started
+## 功能特点
 
-This project is a starting point for a Flutter application.
+- 设备管理和跟踪
+- 设备借用和归还记录
+- 待办事项管理
+- 活动记录
 
-A few resources to get you started if this is your first Flutter project:
+## 开始使用
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+本项目是一个Flutter应用程序。
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+开发环境设置：
+
+- [安装 Flutter](https://docs.flutter.dev/get-started/install)
+- 运行 `flutter pub get` 获取依赖
+- 使用 `flutter run` 启动应用
+
+## 许可证
+
+本项目采用 GPL3.0 许可证 - 详情请查看 [LICENSE](LICENSE) 文件。
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
+/*
+ * Valet - 一个用于设备管理的Flutter应用
+ * Copyright (C) 2025 您的姓名或组织
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -9,6 +27,14 @@ void main() async {
   logger.setLogLevel(kDebugMode ? LogLevel.debug : LogLevel.info);
   logger.enableConsole(true);
   logger.info('应用程序启动', tag: 'App');
+  
+  // 显示版权和许可信息
+  if (kDebugMode) {
+    print('Valet  Copyright (C) 2025 HITCRT');
+    print('This program comes with ABSOLUTELY NO WARRANTY');
+    print('This is free software, and you are welcome to redistribute it');
+    print('under certain conditions; see the LICENSE file for details.');
+  }
 
   // 初始化环境变量
   await dotenv.load(fileName: ".env/dev.env");
